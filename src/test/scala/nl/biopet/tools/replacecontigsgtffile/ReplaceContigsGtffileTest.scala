6a5ddee185a0775f254da87602794a61ef93a62b
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-11 09:50:36 +0200

    object to class

--- src/test/scala/nl/biopet/tools/replacecontigsgtffile/ReplaceContigsGtffileTest.scala
+++ src/test/scala/nl/biopet/tools/replacecontigsgtffile/ReplaceContigsGtffileTest.scala
@@ -3,7 +3,7 @@ package nl.biopet.tools.replacecontigsgtffile
 import nl.biopet.test.BiopetTest
 import org.testng.annotations.Test
 
-object ReplaceContigsGtffileTest extends BiopetTest {
+class ReplaceContigsGtffileTest extends BiopetTest {
   @Test
   def testNoArgs(): Unit = {
     intercept[IllegalArgumentException] {

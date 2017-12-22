c73a1a35314f56be636ff5e9ae17369598333370
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-12-22 13:19:54 +0100

    create headers, scalafmt

--- src/main/scala/nl/biopet/tools/replacecontigsgtffile/ReplaceContigsGtfFile.scala
+++ src/main/scala/nl/biopet/tools/replacecontigsgtffile/ReplaceContigsGtfFile.scala
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2017 Sequencing Analysis Support Core - Leiden University Medical Center
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package nl.biopet.tools.replacecontigsgtffile
 
 import java.io.PrintWriter
@@ -93,57 +114,57 @@ object ReplaceContigsGtfFile extends ToolCommand[Args] {
        |
      """.stripMargin
 
-
   def exampleText: String =
     s"""
        |To convert the contig names in a gtf file with case sensitivity  and output as GFF run:
        |
        |${example("-I",
-      "input.gtf",
-      "-o",
-      "output.gtf",
-      "-R",
-      "reference.fasta",
-      "--contigMappingFile",
-      "contignames.tsv",
-      "--caseSensitive", "--writeAsGff")}
+                  "input.gtf",
+                  "-o",
+                  "output.gtf",
+                  "-R",
+                  "reference.fasta",
+                  "--contigMappingFile",
+                  "contignames.tsv",
+                  "--caseSensitive",
+                  "--writeAsGff")}
        |
        |To convert the contig names using command line options, similar
        |to the example contig mapping file given in the manual:
        |
        |${example(
-      "-I",
-      "input.gtf",
-      "-o",
-      "output.gtf",
-      "-R",
-      "reference.fasta",
-      "--contig",
-      "1=chr1",
-      "--contig",
-      "I=chr1",
-      "--contig",
-      "one=chr1",
-      "--contig",
-      "2=chr2",
-      "--contig",
-      "II=chr2",
-      "--contig",
-      "two=chr2"
-    )}
+         "-I",
+         "input.gtf",
+         "-o",
+         "output.gtf",
+         "-R",
+         "reference.fasta",
+         "--contig",
+         "1=chr1",
+         "--contig",
+         "I=chr1",
+         "--contig",
+         "one=chr1",
+         "--contig",
+         "2=chr2",
+         "--contig",
+         "II=chr2",
+         "--contig",
+         "two=chr2"
+       )}
        |
        | A contig mapping file and contigs can be used together:
        |${example("-I",
-      "input.gtf",
-      "-o",
-      "output.gtf",
-      "-R",
-      "reference.fasta",
-      "--contigMappingFile",
-      "contignames.tsv",
-      "--contig",
-      "3=chr3",
-      "--contig",
-      "III=chr3")}
+                  "input.gtf",
+                  "-o",
+                  "output.gtf",
+                  "-R",
+                  "reference.fasta",
+                  "--contigMappingFile",
+                  "contignames.tsv",
+                  "--contig",
+                  "3=chr3",
+                  "--contig",
+                  "III=chr3")}
      """.stripMargin
 }
